f7f38f107debf55cd1a25dcdc3949d832094b183
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-20 10:55:18 +0200

    project and assets setup

---
+++ OnlineCrypto/Extensions/Color.swift
@@ -0,0 +1,23 @@
+//
+//  Color.swift
+//  OnlineCrypto
+//
+//  Created by Ngoni Katsidzira  on 20/7/2026.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+}
+
+struct ColorTheme {
+    
+    let accent = Color(.accent)
+    let background = Color(.background)
+    let green = Color(.theGreen)
+    let red = Color(.theRed)
+    let secondaryText = Color(.secondaryText)
+}
