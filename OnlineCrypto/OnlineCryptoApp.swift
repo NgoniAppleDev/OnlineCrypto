1622df0475d2026dfeb9676cca7fda38f40970e0
Ngoni AppleDev <ngonikatsidzira@icloud.com>
2026-07-20 10:17:35 +0200

    Initial Commit

---
+++ OnlineCrypto/OnlineCryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineCryptoApp.swift
+//  OnlineCrypto
+//
+//  Created by Ngoni Katsidzira  on 20/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct OnlineCryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
